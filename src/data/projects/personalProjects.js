c0798723f204afe2e615da308e24ef8c4ae576f9
Mirko.2404 <88331123+Sc0rpii@users.noreply.github.com>
2026-09-20 12:56:16 +0200

    implement new personal project aura studio

--- src/data/projects/personalProjects.js
+++ src/data/projects/personalProjects.js
@@ -1,4 +1,5 @@
 import AnticaBraceria from "../../assets/projects/personal/antica-braceria.webp";
+import AuraStudio from "../../assets/projects/personal/AuraStudioHero.webp";
 
 export const personalProjects = [
     {
@@ -11,5 +12,16 @@ export const personalProjects = [
         descriptionKey: "projects.anticaBraceria.description",
         overviewKey: "projects.anticaBraceria.overview",
         url: "https://sc0rpii.github.io/AnticaBrace/",
+    },
+    {
+        id: "aurastudio",
+        img: AuraStudio,
+        width: 1440,
+        height: 848,
+        title: "AURA STUDIO",
+        kind: "personal",
+        descriptionKey: "projects.aurastudio.description",
+        overviewKey: "projects.aurastudio.overview",
+        url: "https://sc0rpii.github.io/hairstyle/",
     }
 ];
